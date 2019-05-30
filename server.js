92d5f5fedb7650ff74f0fe83648762abfc4a9b3c
Caleb <caleb.ziegler@gmail.com>
2019-05-30 13:06:45 -0500

    Add exit survey endpoint.

--- server.js
+++ server.js
@@ -101,18 +101,16 @@ apiRoutes.post('/spoofPost',(req,res) => {
 })
 
 apiRoutes.post('/submitCampSurvey', (req,res) => {
-    console.log(req.body)
     //pull values from request
     receivedData = []
     for (var key in req.body) {
         receivedData.push(req.body[key])
     }
-    console.log(receivedData)
     
     //set up sql insert
     let sqlPost = `INSERT INTO camp_surveys 
-                    (submitDate, grade, branch, status, role, skillSets, skillSetsComments, training, trainingComments, deployedEnv, deployedEnvComments, deployInfo, deployInfoComments)
-                    values (CURRENT_TIMESTAMP, (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?))`
+                    (submitDate, grade, branch, status, role, daysAtExercise, deployedPreviously, supportedPreviously, planningAttendance, skillSets, skillSetsComments, training, trainingComments, deployedEnv, deployedEnvComments, deployInfo, deployInfoComments)
+                    values (CURRENT_TIMESTAMP, (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?))`
     //run insert
     db.run(sqlPost, receivedData, function(err) {
         if (err) {
@@ -130,35 +128,25 @@ apiRoutes.post('/submitCampSurvey', (req,res) => {
     })
 })
 
-apiRoutes.post('/submitTargetRates', formidableMiddleware(), (req,res) => {
-    console.log('beginbegin')
+apiRoutes.post('/submitExitSurvey', (req,res) => {
     //pull values from request
-    var afsc = req.fields.afsc
-    var targetRates = JSON.parse(req.fields.targetRates)
-    var person = req.fields.person
-    //set up sql insert
-    let sqlPost = `INSERT INTO targetRates 
-                    (afsc,tier,criteria,percent,submitDate,submittedBy)
-                    values `
-    //make string for each insert
-    let queryValues = "((?), (?), (?), (?), CURRENT_TIMESTAMP, (?))"
-    var data = []
-    var rowValues = []
-    //make one dimensional array for query and data elements
-    for (let i = 0; i < targetRates.length; i++) {
-        rowValues.push(queryValues)
-        data.push(afsc)
-        data.push(targetRates[i].tier)
-        data.push(targetRates[i].criteria)
-        data.push(targetRates[i].percent)
-        data.push(person)
+    receivedData = []
+    for (var key in req.body) {
+        receivedData.push(req.body[key])
     }
-    //make large insert string
-    sqlPost += rowValues.join(", ");
-
-    db.run(sqlPost, data, function(err) {
+    
+    //set up sql insert
+    let sqlPost = `INSERT INTO exit_surveys 
+                    (submitDate, grade, branch, status, role, daysAtExercise, deployedPreviously, supportedPreviously, planningAttendance, deployAbility, deployAbilityComments, conductingForeign, conductingForeignComments, training, trainingComments, deployedEnv, deployedEnvComments)
+                    values (CURRENT_TIMESTAMP, (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?))`
+    //run insert
+    db.run(sqlPost, receivedData, function(err) {
         if (err) {
-            throw err
+            //send error back
+            res.status(400).send({
+                success: false,
+                message: 'Error attempting to submit data.'
+            })
         } else {
             res.status(200).send({
                 success: true,
@@ -167,6 +155,7 @@ apiRoutes.post('/submitTargetRates', formidableMiddleware(), (req,res) => {
         }
     })
 })
+
 
 
 app.use('/api', apiRoutes)
@@ -188,37 +177,3 @@ app.listen(port)
 
 console.log('Server up at http://localhost:' + port)
 
-function dtSAStoJS(dtSAS,dtType='DATE'){
-  // accepts SAS unformatted DATE or DATETIME
-  // dtType should be used to determine the above
-  // -315619200000 is equivalent to +new Date(1960,0,1)
-  // 86400000 is equivalent to 24h * 60m * 60s * 1000ms
-  if(dtType==='DATE'){
-    return new Date(-315619200000 + dtSAS * 86400000);
-  } else if (dtType==='DATETIME'){
-    return new Date(-315619200000 + dtSAS * 1000);
-  } else {
-    console.log('Unknown dtType value - ' + dtType);
-    return null;
-  }
-};
-
-
-function formatSASDate(sasdate) {
-    if (sasdate){
-        var date = new Date(-315619200000 + sasdate * 86400000)
-
-        var year = date.getFullYear();
-
-        var month = (1 + date.getMonth()).toString();
-        month = month.length > 1 ? month : '0' + month;
-
-        var day = date.getDate().toString();
-        day = day.length > 1 ? day : '0' + day;
-
-        return year + '/' + month + '/' + day;
-    }
-    else {
-        return ""
-    }
-}
