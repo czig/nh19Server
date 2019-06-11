e677a677439d6e435ac5866dc6123e2750a8df8b
Caleb <caleb.ziegler@gmail.com>
2019-06-11 11:13:16 -0500

    Apply format to grade to convert specific grade to grade range.

--- combine_db.py
+++ combine_db.py
@@ -5,25 +5,48 @@ engine = create_engine('sqlite:///./surveys.db')
 engine2 = create_engine('sqlite:///./surveys2.db')
 out_engine = create_engine('sqlite:///./combined_surveys.db')
 
+grade_dict = {
+'E1': 'E1-E4', 
+'E2': 'E1-E4',
+'E3': 'E1-E4',
+'E4': 'E1-E4',
+'E5': 'E5-E6',
+'E6': 'E5-E6',
+'E7': 'E7-E9',
+'E8': 'E7-E9',
+'E9': 'E7-E9',
+'O1': 'O1-O3',
+'O2': 'O1-O3',
+'O3': 'O1-O3',
+'O4': 'O4-O6',
+'O5': 'O4-O6',
+'O6': 'O4-O6',
+}
+
 entry_df = pd.read_sql("""select * from entry_surveys""", engine)
 camp_df = pd.read_sql("""select * from camp_surveys""",engine)
 exit_df = pd.read_sql("""select * from exit_surveys""",engine)
 print('first entry surveys')
-print(entry_df)
+print(exit_df['grade'])
 
 entry_df2 = pd.read_sql("""select * from entry_surveys""", engine2)
 camp_df2 = pd.read_sql("""select * from camp_surveys""",engine2)
 exit_df2 = pd.read_sql("""select * from exit_surveys""",engine2)
 
 print('second entry surveys')
-print(entry_df2)
+print(exit_df2['grade'])
 
+#combine databases into one
 all_entry = pd.concat([entry_df, entry_df2])
 all_camp = pd.concat([camp_df, camp_df2])
 all_exit = pd.concat([exit_df, exit_df2])
 
+all_entry['grade'].replace(grade_dict, inplace=True)
+all_camp['grade'].replace(grade_dict, inplace=True)
+all_exit['grade'].replace(grade_dict, inplace=True)
+
 print('combined entry surveys')
-print(all_entry)
+print(all_exit['grade'])
 
 all_entry.to_sql('entry_surveys',con=out_engine,if_exists="replace")
 all_camp.to_sql('camp_surveys',con=out_engine,if_exists="replace")
